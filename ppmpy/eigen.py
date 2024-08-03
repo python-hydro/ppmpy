344ca545dc54966473db138fc7a5de63a5aa323b
Michael Zingale <michael.zingale@stonybrook.edu>
2024-08-02 21:17:08 -0400

    start on the Euler solver

---
+++ ppmpy/eigen.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def eigen(q, v, gamma):
+    """Compute the left and right eigenvectors and the eigenvalues for
+    the Euler equations.  q is a single zone primitive variable state,
+    v is a FluidVars object that identifies the components of q
+
+    """
+
+    assert v.nvar == 3
+
+    cs = np.sqrt(gamma * q[v.qp] / q[v.qrho])
+
+    # The Jacobian matrix for the primitive variable formulation of the
+    # Euler equations is
+    #
+    #       / u   r   0   \
+    #   A = | 0   u   1/r |
+    #       \ 0  rc^2 u   /
+    #
+    # With the rows corresponding to rho, u, and p
+    #
+    # The eigenvalues are u - c, u, u + c
+
+    rho = q[v.qrho]
+    u = q[v.qu]
+    p = q[v.qp]
+
+    eval = np.array([u - cs, u, u + cs])
+
+
+    # The left eigenvectors are
+    #
+    #   l1 =     ( 0,  -r/(2c),  1/(2c^2) )
+    #   l2 =     ( 1,     0,     -1/c^2,  )
+    #   l3 =     ( 0,   r/(2c),  1/(2c^2) )
+    #
+
+    lvec = np.array([[0.0, -0.5 * rho / cs, 0.5 / cs**2],  # u - c
+                     [1.0, 0.0, -1.0 / cs**2 ],  # u
+                     [0.0, 0.5 * rho / cs, 0.5 / cs**2]])  # u + c
+
+    # The right eigenvectors are
+    #
+    #       /  1  \        / 1 \        /  1  \
+    # r1 =  |-c/r |   r2 = | 0 |   r3 = | c/r |
+    #       \ c^2 /        \ 0 /        \ c^2 /
+    #
+
+    rvec = np.array([[1.0, -cs / rho, cs**2],  # u - c
+                     [1.0, 0.0, 0.0],  # u
+                     [1.0, cs / rho, cs**2]])  # u + c
+
+    return eval, lvec, rvec
