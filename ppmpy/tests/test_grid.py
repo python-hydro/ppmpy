5bdb33667b8df8cc9746e427ee6f09ef55db07e3
Michael Zingale <michael.zingale@stonybrook.edu>
2024-08-04 17:13:33 -0400

    add a test of the grid

---
+++ ppmpy/tests/test_grid.py
@@ -0,0 +1,45 @@
+import numpy as np
+from numpy.testing import assert_array_equal, assert_array_almost_equal_nulp
+
+from ppmpy.grid import FVGrid
+
+
+class TestGrid:
+
+    @classmethod
+    def setup_class(cls):
+        """ this is run once for each class before any tests """
+
+    @classmethod
+    def teardown_class(cls):
+        """ this is run once for each class after all tests """
+
+    def setup_method(self):
+        """ this is run before each test """
+
+        self.g = FVGrid(5, ng=2, xmin=-10, xmax=10)
+
+    def teardown_method(self):
+        """ this is run after each test """
+
+    def test_properties(self):
+
+        assert self.g.nx == 5
+        assert self.g.ng == 2
+        assert self.g.nq == 9
+
+    def test_indices(self):
+
+        assert self.g.lo == 2
+        assert self.g.hi == 6
+
+    def test_coords(self):
+
+        assert_array_equal(self.g.x, np.array([-16., -12.,  -8.,  -4.,   0.,   4.,   8.,  12.,  16.]))
+        assert_array_equal(self.g.xl, np.array([-18., -14., -10.,  -6.,  -2.,   2.,   6.,  10.,  14.]))
+        assert_array_equal(self.g.xr, np.array([-14., -10.,  -6.,  -2.,   2.,   6.,  10.,  14., 18.]))
+
+        assert_array_almost_equal_nulp(self.g.x, 0.5 * (self.g.xl + self.g.xr))
+
+        assert self.g.xl[self.g.lo] == -10.0
+        assert self.g.xr[self.g.hi] == 10.0
