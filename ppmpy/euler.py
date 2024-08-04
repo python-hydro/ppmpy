59c82abfdd0e60ec5d0c729a91cd693051f1e49a
Michael Zingale <michael.zingale@stonybrook.edu>
2024-08-03 20:14:51 -0400

    some fixes to Euler kinetic energy was computed wrong beta's should not accumulate

--- ppmpy/euler.py
+++ ppmpy/euler.py
@@ -56,7 +56,7 @@ class Euler:
         q[:, self.v.qrho] = self.U[:, self.v.urho]
         q[:, self.v.qu] = self.U[:, self.v.umx] / self.U[:, self.v.urho]
 
-        rhoe = self.U[:, self.v.uener] - 0.5 * q[:, self.v.qu]**2
+        rhoe = self.U[:, self.v.uener] - 0.5 * q[:, self.v.qrho] * q[:, self.v.qu]**2
         q[:, self.v.qp] = rhoe * (self.gamma - 1.0)
 
         return q
@@ -114,21 +114,21 @@ class Euler:
 
             # loop over waves and compute l . (qref - I) for each wave
             for iwave in range(3):
-                beta_xm[iwave] += lvec[iwave, :] @ (q_ref_m - Im[i, iwave, :])
-                beta_xp[iwave] += lvec[iwave, :] @ (q_ref_p - Ip[i, iwave, :])
+                beta_xm[iwave] = lvec[iwave, :] @ (q_ref_m - Im[i, iwave, :])
+                beta_xp[iwave] = lvec[iwave, :] @ (q_ref_p - Ip[i, iwave, :])
 
             # finally sum up the waves moving toward the interface, accumulating (l . (q_ref - I)) r
             q_left[i+1, :] = q_ref_p[:]
             q_right[i, :] = q_ref_m[:]
             for iwave in range(3):
-                if ev[iwave] >= 0:
+                if ev[iwave] > 0:
                     q_left[i+1, :] -= beta_xp[iwave] * rvec[iwave, :]
-                if ev[iwave] <= 0:
+                if ev[iwave] < 0:
                     q_right[i, :] -= beta_xm[iwave] * rvec[iwave, :]
 
             # godunov hack
-            q_left[i+1, :] = q[i, :]
-            q_right[i, :] = q[i, :]
+            #q_left[i+1, :] = q[i, :]
+            #q_right[i, :] = q[i, :]
 
         return q_left, q_right
 
