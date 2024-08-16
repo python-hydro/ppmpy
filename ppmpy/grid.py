ddbeb528925a636e1a54fd10da8645399fe02e68
Michael Zingale <michael.zingale@stonybrook.edu>
2024-08-16 13:09:36 -0400

    better API styling (#32)
    this uses the numpydoc standard

--- ppmpy/grid.py
+++ ppmpy/grid.py
@@ -8,7 +8,20 @@ import matplotlib.pyplot as plt
 
 
 class GridPlot:
-    """a container to hold info about the grid figure"""
+    """a container to hold info about the grid figure
+
+    Parameters
+    ----------
+    fig : matplotlib.pyplot.Figure
+         the figure object.
+    ax : matplotlib.pyplot.Axes
+         the axis object.
+    lo_index : int, optional
+         the 0-based zone index for the left edge of the plot.
+    hi_index : int, optional
+         the 0-based zone index for the right edge of the plot.
+    """
+
     def __init__(self, *, fig=None, ax=None, lo_index=None, hi_index=None):
         self.fig = fig
         self.ax = ax
@@ -16,12 +29,29 @@ class GridPlot:
         self.hi_index = hi_index
 
     def show_fig(self):
-        """return the Figure object"""
+        """return the Figure object
+
+        Returns
+        -------
+        matplotlib.pyplot.Figure
+        """
         return self.fig
 
 
 class FVGrid:
-    """The main finite-volume grid class for holding our fluid state."""
+    """The main finite-volume grid class for holding our fluid state.
+
+    Parameters
+    ----------
+    nx : int
+        number of zones on the grid.
+    ng : int
+        number of ghost cells on each end of the grid.
+    xmin : float, optional
+        minimum x-coordinate.
+    xmax : float, optional
+        maximum x-coordinate.
+    """
 
     def __init__(self, nx, ng, *,
                  xmin=0.0, xmax=1.0):
@@ -43,13 +73,33 @@ class FVGrid:
         self.x = xmin + (np.arange(nx+2*ng)-ng+0.5)*self.dx
 
     def scratch_array(self, nc=1):
-        """ return a scratch array dimensioned for our grid """
+        """ return a scratch array dimensioned for our grid
+
+        Parameters
+        ----------
+        nc : int
+            number of components.
+
+        Returns
+        -------
+        ndarray
+        """
         return np.squeeze(np.zeros((self.nq, nc), dtype=np.float64))
 
     def ghost_fill(self, atmp, *,
                    bc_left_type="outflow", bc_right_type="outflow"):
         """fill all ghost cells with zero-gradient boundary
-        conditions"""
+        conditions.
+
+        Parameters
+        ----------
+        atmp : ndarray
+             the array of data defined on the `FVGrid` to fill ghost cells in
+        bc_left_type : string
+             boundary condition type on the left edge of the domain.
+        bc_right_type : string
+             boundary condition type on the left edge of the domain.
+        """
 
         # left
 
@@ -83,7 +133,18 @@ class FVGrid:
             raise ValueError("invalid boundary condition")
 
     def norm(self, e):
-        """compute the L2 norm of array e defined on this grid"""
+        """compute the L2 norm of array e defined on this grid
+
+        Parameters
+        ----------
+        e : ndarray
+             array of data defined on the grid whose norm we want to
+             compute
+
+        Returns
+        -------
+        float
+        """
 
         assert len(e) == self.nq
         return np.sqrt(self.dx * np.sum(e[self.lo:self.hi+1]**2))
@@ -91,7 +152,19 @@ class FVGrid:
     def coarsen(self, fdata):
         """coarsen an array fine defined on this grid down to a grid
         with 1/2 the number of zones (but the same number of ghost
-        cells
+        cells.
+
+        Parameters
+        ----------
+        fdata : ndarray
+            The data defined on this `FVGrid` object.
+
+        Returns
+        -------
+        FVGrid
+            The coarse grid object.
+        ndarray
+            The coarsened data on the coarse grid.
 
         """
 
@@ -106,7 +179,22 @@ class FVGrid:
 
     def draw(self, *, lo_index=None, hi_index=None, stretch=1):
         """Draw a finite volume representation of the grid and return the
-        figure and axis objects"""
+        figure and axis objects
+
+
+        Parameters
+        ----------
+        lo_index : int
+            0-based zone index to start the grid plot.
+        hi_index : int
+            0-based zone index to end the grid plot.
+        stretch : float
+            factor by which to stretch the vertical axis
+
+        Returns
+        -------
+        GridPlot
+        """
 
         fig, ax = plt.subplots()
 
@@ -119,6 +207,8 @@ class FVGrid:
             nstop = self.hi + self.ng
         else:
             nstop = hi_index
+
+        assert nstop > nstart
 
         ax.plot([self.xl[nstart], self.xr[nstop]], [0, 0], color="0.25", lw=2)
 
